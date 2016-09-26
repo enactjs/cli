a128b6660d75daef8602e56c01ae6e8be1b71c9a
Aaron Tam <aarontam@users.noreply.github.com>
2016-09-26 15:18:41 -0700

    PLAT-27045: Updates initial app template (#1)
    * Updates initial app template
    
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    * Revises app template
    
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    * Tweaks the package.json and some clean up
    
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    * Add directory README files
    
    Issue: PLAT-27045
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>
    
    * Fix ilib enyo reference to enact
    
    * Remove peerDependency for enact-dev from template
    
    * Add directory README files
    
    Issue: PLAT-27045
    Enyo-DCO-1.1-Signed-off-by: Aaron Tam <aaron.tam@lge.com>

--- global-cli/template/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import css from './App.less';
-
-export default class App extends React.Component {
-	render () {
-		return <div className={css.app}>Hello World!</div>;
-	}
-}
