6a2c98a2a170c433ba71aed8f59254bd3e4323de
Jason Robitaille <robitaille88@gmail.com>
2016-09-27 16:21:34 -0700

    Exit with child process error code. (#7)

--- global-cli/pack.js
+++ global-cli/pack.js
@@ -6,5 +6,8 @@ module.exports = function(args) {
 	if(!utils.exists('./webpack.config.js')) {
 		args.unshift('--config', require.resolve('./internal/webpack.js'));
 	}
-	cp.fork(require.resolve('webpack/bin/webpack'), args, {env:process.env, cwd:process.cwd()});
+	var child = cp.fork(require.resolve('webpack/bin/webpack'), args, {env:process.env, cwd:process.cwd()});
+	child.on('close', function(code, signal) {
+		process.exit(code);
+	});
 };
