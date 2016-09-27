79d6ba77a959db52eafe6f20fe24a0c01657076a
Derek Tor <derek.tor@lge.com>
2016-09-27 11:14:46 -0700

    Converted propTypeChecker to es5
    Enyo-DCO-1.1-Signed-off-by: Derek Tor derek.tor@lge.com

--- src/proptype-checker.js
+++ src/proptype-checker.js
@@ -1,20 +1,18 @@
-/* global console beforeEach afterEach*/
+/* global console */
 /* eslint no-console: ["error", { allow: ["error"] }] */
 
-import {
-	watchErrorAndWarnings,
-	filterErrorAndWarnings,
-	restoreErrorAndWarnings
-} from 'enyo-console-spy';
+var watchErrorAndWarnings = require('enyo-console-spy').watchErrorAndWarnings,
+	filterErrorAndWarnings = require('enyo-console-spy').filterErrorAndWarnings,
+	restoreErrorAndWarnings = require('enyo-console-spy').restoreErrorAndWarnings;
 
 beforeEach(watchErrorAndWarnings);
 
 afterEach(function (done) {
-	const actual = filterErrorAndWarnings(/(Invalid prop|Failed prop type|Unknown prop)/);
-	const expected = 0;
+	var actual = filterErrorAndWarnings(/(Invalid prop|Failed prop type|Unknown prop)/);
+	var expected = 0;
 	restoreErrorAndWarnings();
 	if (actual.length > expected) {
-		console.error(`PropType Failure: ${this.currentTest.parent.title} at "${this.currentTest.title}"`);
+		console.error('PropType Failure: ' + this.currentTest.parent.title + ' at "' + this.currentTest.title + '"');
 	}
 	done();
 	expect(actual).to.have.length(expected);
