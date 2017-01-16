ce00e7a57d75026517c79e2a67e505cedc30ed8a
Jason Robitaille <jason.robitaille@lge.com>
2017-01-16 11:10:00 -0800

    ENYO-3803: Add config option for node polyfills

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -74,12 +74,12 @@ module.exports = function(karma) {
 				'react/lib/ExecutionEnvironment': true,
 				'react/lib/ReactContext': true
 			},
-			node: {
+			node: Object.assign({}, enact.node|| {}, {
 				console: true,
 				fs: 'empty',
 				net: 'empty',
 				tls: 'empty'
-			},
+			}),
 			module: {
 				loaders: [
 					{test: /\.(js|jsx|es6)$/, loader: 'babel', exclude: /node_modules.(?!@enact)/,
