2a94f4f928ed04e9007cfefb757b7aed65d97325
Jason Robitaille <jason.robitaille@lge.com>
2017-03-06 15:19:07 -0800

    Generate an empty content shell as default non-localized prerender case

--- global-cli/modifiers/util/PrerenderPlugin.js
+++ global-cli/modifiers/util/PrerenderPlugin.js
@@ -6,6 +6,11 @@ var
 	LocaleHtmlPlugin = require('./LocaleHtmlPlugin'),
 	FileXHR = require('./FileXHR');
 
+// RegExp for prerender cleaning in content shell mode
+var iconRegExp = /class=["]([^"]* )*Icon__icon__/;
+// Equivalent to /[ \S]/gu transpiled via regexpu to ES5. Can be swapped once NodeJS 6.x is new minimum requirement.
+var spaceRegExp =  /(?:[\0-\x08\x0E-\x9F\xA1-\u167F\u1681-\u1FFF\u200B-\u2027\u202A-\u202E\u2030-\u205E\u2060-\u2FFF\u3001-\uD7FF\uE000-\uFEFE\uFF00-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/g;
+
 require('console.mute');
 
 // Determine if it's a NodeJS output filesystem or if it's a foreign/virtual one.
@@ -18,15 +23,26 @@ function isNodeOutputFS(compiler) {
 
 function PrerenderPlugin(options) {
 	this.options = options || {};
+	this.options.chunk = this.options.chunk || 'main.js';
 }
 
 PrerenderPlugin.prototype.apply = function(compiler) {
 	var opts = this.options;
+	var src;
+
 	compiler.plugin('compilation', function(compilation) {
 		if(isNodeOutputFS(compiler)) {
+			// Get the chunk source before any optimizations for prerender usage
+			compilation.plugin('chunk-asset', function(chunk, file, callback) {
+				if(file === opts.chunk) {
+					src = compilation.assets[opts.chunk].source();
+				}
+				callback && callback();
+			});
+
 			compilation.plugin('html-webpack-plugin-after-html-processing', function(params, callback) {
 				var htmlTemplate = params.html;
-				var appFile = path.join(compiler.context, compiler.options.output.path, 'main.js');
+				var appFile = path.join(compiler.context, compiler.options.output.path, opts.chunk);
 
 				// Attempt to resolve 'react-dom/server' relative to the project itself with internal as fallback
 				var ReactDOMServer;
@@ -44,7 +60,6 @@ PrerenderPlugin.prototype.apply = function(compiler) {
 					global.Request = global.fetch.Request;
 				}
 				try {
-					var src = compilation.assets['main.js'].source();
 					if(params.plugin.options.externalFramework) {
 						// Add external Enact framework filepath if it's used
 						src = src.replace(/require\(["']enact_framework["']\)/g, 'require("' + params.plugin.options.externalFramework +  '")');
@@ -54,14 +69,25 @@ PrerenderPlugin.prototype.apply = function(compiler) {
 						global.iLibLocale = framework('@enact/i18n/src/locale');
 					}
 					console.mute();
-					var App = requireFromString(src, 'main.js');
-					var code;
+					var App, code, htmlAppend = '';
 
 					if(opts.locales) {
-						if(global.iLibLocale && global.iLibLocale.updateLocale) {
-							global.iLibLocale.updateLocale('xx-XX');
-						}
-						code = ReactDOMServer.renderToString(App['default'] || App);
+						// Hotwire $L to return empty strings of a matching length
+						App = requireFromString(src.replace('function $L(str) {',
+								'function $L(str) { return " ".repeat((str.value || str).length);'), opts.chunk);
+
+						// Append a script tag just below the prerendered HTML which can apply the RTL locale class to reduce the amount of 
+						// visual changes when the app renders over the content shell
+						htmlAppend = '\n\t\t<script type="text/javascript">if(document.documentElement.dir==="rtl") {'
+								+ 'document.getElementById("root").className += " enact-locale-right-to-left"; }</script>';
+
+						code = ReactDOMServer.renderToStaticMarkup(App['default'] || App).replace(/(<[^<]*>)([^<>]+)</gu, function(match, tag, content) {
+							if(iconRegExp.test(tag)) {
+								return match;
+							} else {
+								return tag + content.replace(spaceRegExp, '&nbsp;') + '<'
+							}
+						});
 
 						compiler.apply(new LocaleHtmlPlugin({
 							locales: opts.locales,
@@ -70,10 +96,11 @@ PrerenderPlugin.prototype.apply = function(compiler) {
 							code: src
 						}));
 					} else {
+						App = requireFromString(src, opts.chunk);
 						code = ReactDOMServer.renderToString(App['default'] || App);
 					}
 					console.resume();
-					params.html = htmlTemplate.replace('<div id="root"></div>', '<div id="root">' + code + '</div>');
+					params.html = htmlTemplate.replace('<div id="root"></div>', '<div id="root">' + code + '</div>' + htmlAppend);
 				} catch(e) {
 					console.log();
 					console.log(chalk.yellow('Unable to generate prerender of app state HTML'));
