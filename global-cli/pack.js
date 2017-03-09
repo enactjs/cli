31d229bca1a42db23c0d87b8599cf6e806003802
Roy Sutton <roy.sutton@lge.com>
2017-03-09 15:03:26 -0500

    Change warning to yellow
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- global-cli/pack.js
+++ global-cli/pack.js
@@ -101,7 +101,7 @@ function build (config, previousSizeMap) {
 		console.log();
 		console.log(chalk.green('Compiled successfully.'));
 		if (process.env.NODE_ENV === 'development') {
-			console.log(chalk.gray('NOTICE: This build contains debugging functionality and may run' +
+			console.log(chalk.yellow('NOTICE: This build contains debugging functionality and may run' +
 					' slower than in production mode.'));
 		}
 		console.log();
