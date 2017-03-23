9f44ba2079f3b52ae8445c865d07a57a56ebef17
Jason Robitaille <jason.robitaille@lge.com>
2017-03-23 14:03:25 -0700

    Removed obsolete fallback code. If isomorphic entrypoints don't update to self-render, apps must be updated.

--- global-cli/modifiers/util/prerendered-startup.js
+++ global-cli/modifiers/util/prerendered-startup.js
@@ -24,19 +24,7 @@
 					// Add script nodes, loading the chunks sequentially.
 					var count = 0;
 					var appendScripts = function(js) {
-						if(js.length===0) {
-							if(typeof enact_framework !== 'undefined') {
-								window.ReactDOM = enact_framework('react-dom');
-							}
-							if((typeof App === 'object') && (typeof ReactDOM === 'object')) {
-								var appEle = (App && App.__esModule) ? App['default'] : App;
-								if(typeof appEle === 'object' && appEle) {
-									console.warn('WARNING: HTML-side isomorphic rendering is depreciated and will be removed in a future release. Please conditionally render within the app itself.');
-									console.warn('    See https://github.com/enyojs/enact-dev/blob/develop/template/src/index.js for an example entrypoint.');
-									ReactDOM.render(appEle, document.getElementById('root'));
-								}
-							}
-						} else {
+						if(js.length>0) {
 							var src = js.shift();
 							var script = document.createElement('script');
 							script.type = 'text/javascript';
