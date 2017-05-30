33ff9c0e1ab5c7caf5f063d9eba7085b6d700300
Jason Robitaille <jason.robitaille@lge.com>
2017-05-30 13:31:59 -0700

    Validate active Node version is compatible with enact-dev

--- bin/enact.js
+++ bin/enact.js
@@ -2,6 +2,17 @@
 
 'use strict';
 
+// Verify the correct version of Node is in use.
+const minimum = [6, 4, 0];
+const active = process.versions.node.split('.').map(val => parseInt(val));
+if(active[0] < minimum[0] || (active[0] === minimum[0] && active[1] < minimum[1])) {
+	const chalk = require('chalk');
+	console.log(chalk.red('You are running Node ' + active.join('.') + '.\nenact-dev requires Node '
+			+ minimum.join('.') + ' or higher. \n' + chalk.bold('Please update your version of Node.')));
+	process.exit(1);
+}
+
+// Handle tasks/arguments
 if (process.argv.indexOf('-v') >= 0 || process.argv.indexOf('--version') >= 0) {
 	const pkg = require('../package.json');
 	console.log(pkg.name);
