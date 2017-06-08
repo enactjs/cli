026765545e9f7b3396c78012ad9b28e03012df2f
Jason Robitaille <jason.robitaille@lge.com>
2017-06-08 11:55:31 -0700

    PLAT-41856: support env var resolving of locale bundles in prerendering

--- global-cli/modifiers/util/FileXHR.js
+++ global-cli/modifiers/util/FileXHR.js
@@ -16,6 +16,10 @@ FileXHR.prototype.addEventListener = function(evt, fn) {
 
 FileXHR.prototype.send = function() {
 	if(this.method.toUpperCase() === 'GET' && this.uri && this.sync) {
+		if(process.env.ILIB_BASE_PATH) {
+			this.uri = this.uri.replace(new RegExp('^' + process.env.ILIB_BASE_PATH),
+					'node_modules/@enact/i18n/ilib');
+		}
 		const parsedURI = this.uri.replace(/\\/g, '/').replace(/^(_\/)+/g, (match) => match.replace(/_/g, '..'));
 		try {
 			if(!exists(parsedURI)) throw new Error('File not found: ' + this.uri);
