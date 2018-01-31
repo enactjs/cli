f3e319b94fe23399133a3c238b41a0e4aea6d6ab
Stephen Choi <viodragon2@gmail.com>
2018-01-31 11:47:28 -0800

    Update enact-cli to react 16 (#109)
    * Initial React 16 upgrade process.
    
    * Remove react-addons-perf
    
    * Update template package.json
    
    * Support EnzymeAdapterPlugin in karma to automate react-16 Enzyme adapter initialization.
    
    * Version updates
    
    * Remove trailing space for linter

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -42,7 +42,6 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const app = require('@enact/dev-utils/option-parser');
 
 process.chdir(app.context);
-const reactPerf = path.resolve(path.join('node_modules', 'react-dom', 'lib', 'ReactPerf.js'));
 
 // This is the development configuration.
 // It is focused on developer experience and fast rebuilds.
@@ -59,8 +58,6 @@ module.exports = {
 		main: [
 			// Include a few polyfills by default (Promise, Object.assign, and fetch)
 			require.resolve('./polyfills'),
-			// Include React performance tools for debugging/optimization testing
-			reactPerf,
 			// Finally, this is your app's code
 			path.resolve(app.main || 'index.js')
 		]
@@ -203,13 +200,6 @@ module.exports = {
 						}
 					]
 				})
-			},
-			// Expose the 'react-addons-perf' on a global context for debugging.
-			// Currently maps the toolset to window.ReactPerf.
-			{
-				test: reactPerf,
-				loader: require.resolve('expose-loader'),
-				options: 'ReactPerf'
 			}
 			// ** STOP ** Are you adding a new loader?
 			// Remember to add the new extension(s) to the "file" loader exclusion regex list.
