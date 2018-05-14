451f5ade7fef883f9dbbec8ef0a73a07597a0762
Jason Robitaille <robitaille88@gmail.com>
2018-05-14 13:26:33 -0700

    Endline spacing formatting normalization

--- commands/create.js
+++ commands/create.js
@@ -9,7 +9,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 const os = require('os');
 const path = require('path');
 const chalk = require('chalk');
--- commands/eject.js
+++ commands/eject.js
@@ -9,7 +9,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 const cp = require('child_process');
 const os = require('os');
 const path = require('path');
