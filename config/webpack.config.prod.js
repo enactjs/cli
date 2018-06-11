cf58158a0c39e468e19edf313ae17a962d0b3957
Jason Robitaille <jason.robitaille@lge.com>
2018-06-11 13:06:03 -0700

    Support Google Closure Compiler

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -22,7 +22,7 @@ const globalImport = require('postcss-global-import');
 const removeclass = require('postcss-remove-classes').default;
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const LessPluginRi = require('resolution-independence');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ClosurePlugin = require('closure-webpack-plugin');
 const {DefinePlugin} = require('webpack');
 const {optionParser: app, GracefulFsPlugin, ILibPlugin, WebOSMetaPlugin} = require('@enact/dev-utils');
 
@@ -226,38 +226,8 @@ module.exports = {
 			}
 		}),
 		// Minify the code.
-		new UglifyJsPlugin({
-			uglifyOptions: {
-				parse: {
-					// we want uglify-js to parse ecma 8 code. However, we don't want it
-					// to apply any minfication steps that turns valid ecma 5 code
-					// into invalid ecma 5 code. This is why the 'compress' and 'output'
-					// sections only apply transformations that are ecma 5 safe
-					// https://github.com/facebook/create-react-app/pull/4234
-					ecma: 8
-				},
-				compress: {
-					ecma: 5,
-					warnings: false,
-					// Disabled because of an issue with Uglify breaking seemingly valid code:
-					// https://github.com/facebook/create-react-app/issues/2376
-					// Pending further investigation:
-					// https://github.com/mishoo/UglifyJS2/issues/2011
-					comparisons: false
-				},
-				output: {
-					ecma: 5,
-					comments: false,
-					// Turned on because emoji and regex is not minified properly using default
-					// https://github.com/facebook/create-react-app/issues/2488
-					ascii_only: true
-				}
-			},
-			// Use multi-process parallel running to improve the build speed
-			// Default number of concurrent runs: os.cpus().length - 1
-			parallel: true,
-			// Enable file caching
-			cache: true
+		new ClosurePlugin({
+			mode: 'STANDARD'
 		}),
 		// Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
 		new ExtractTextPlugin('[name].css'),
