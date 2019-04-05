ac2016b326fae3bff0395e1629abb8352e32ae88
Jason Robitaille <robitaille88@gmail.com>
2019-04-05 12:23:16 -0700

    Uses `npm config get prefix -g` to resolve global module path to allow for custom paths via npmrc configs. (#178)

--- commands/link.js
+++ commands/link.js
@@ -3,7 +3,6 @@ const path = require('path');
 const chalk = require('chalk');
 const spawn = require('cross-spawn');
 const fs = require('fs-extra');
-const globalDir = require('global-modules');
 const minimist = require('minimist');
 const packageRoot = require('@enact/dev-utils').packageRoot;
 
@@ -19,36 +18,61 @@ function displayHelp() {
 	process.exit(0);
 }
 
+function globalModules() {
+	return new Promise(resolve => {
+		let prefix = '';
+		const proc = spawn('npm', ['config', 'get', 'prefix', '-g'], {
+			stdio: 'pipe',
+			cwd: process.cwd(),
+			env: process.env
+		});
+		proc.stdout.on('data', data => {
+			prefix += data.toString().replace(/\n/g, '');
+		});
+		proc.on('close', code => {
+			if (code !== 0 || !prefix) {
+				resolve(require('global-modules'));
+			} else if (process.platform === 'win32' || ['msys', 'cygwin'].includes(process.env.OSTYPE)) {
+				resolve(path.resolve(prefix, 'node_modules'));
+			} else {
+				resolve(path.resolve(prefix, 'lib/node_modules'));
+			}
+		});
+	});
+}
+
 function api({verbose = false} = {}) {
 	const linkArgs = ['--loglevel', verbose ? 'verbose' : 'error', 'link'];
 	const pkg = packageRoot();
 	let enact = Object.keys(pkg.meta.dependencies || {}).concat(Object.keys(pkg.meta.devDependencies || {}));
 	enact = enact.filter(d => d.startsWith('@enact/')).map(d => d.replace('@enact/', ''));
 
-	return new Promise((resolve, reject) => {
-		const missing = [];
-		for (let i = 0; i < enact.length; i++) {
-			if (fs.existsSync(path.join(globalDir, '@enact', enact[i]))) {
-				linkArgs.push('@enact/' + enact[i]);
-			} else {
-				missing.push('@enact/' + enact[i]);
-			}
-		}
-
-		if (enact.length === 0) {
-			reject(new Error('No Enact dependencies found within the package. Nothing to link.'));
-		} else if (missing.length === enact.length) {
-			reject(new Error('Unable to detect any Enact global modules. Please ensure they are linked correctly.'));
-		} else {
-			const proc = spawn('npm', linkArgs, {stdio: 'inherit', cwd: process.cwd()});
-			proc.on('close', code => {
-				if (code !== 0) {
-					reject(new Error('"npm ' + linkArgs.join(' ') + '" failed'));
+	return globalModules().then(globalDir => {
+		return new Promise((resolve, reject) => {
+			const missing = [];
+			for (let i = 0; i < enact.length; i++) {
+				if (fs.existsSync(path.join(globalDir, '@enact', enact[i]))) {
+					linkArgs.push('@enact/' + enact[i]);
 				} else {
-					resolve();
+					missing.push('@enact/' + enact[i]);
 				}
-			});
-		}
+			}
+
+			if (enact.length === 0) {
+				reject(new Error('No Enact dependencies found within the package. Nothing to link.'));
+			} else if (missing.length === enact.length) {
+				reject(new Error('Enact global modules not found. Ensure they are linked correctly.'));
+			} else {
+				const proc = spawn('npm', linkArgs, {stdio: 'inherit', cwd: process.cwd()});
+				proc.on('close', code => {
+					if (code !== 0) {
+						reject(new Error('"npm ' + linkArgs.join(' ') + '" failed'));
+					} else {
+						resolve();
+					}
+				});
+			}
+		});
 	});
 }
 
