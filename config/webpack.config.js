4170360e55194d3ba03e459cbc963eaa6228de43
Mikyung Kim <mikyung27.kim@lge.com>
2021-02-03 17:05:10 +0900

    PLAT-121645: Add support for new JSX Transform (#236)
    * PLAT-121645: Add support for new JSX Transform
    
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)
    
    * fix lint error
    
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)
    
    * fix lint
    
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)
    
    * Added a guard
    
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -47,6 +47,20 @@ module.exports = function (env) {
 	// Sets the browserslist default fallback set of browsers to the Enact default browser support list.
 	app.setEnactTargetsAsDefault();
 
+	// Check if JSX transform is able
+	const hasJsxRuntime = (() => {
+		if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+			return false;
+		}
+
+		try {
+			require.resolve('react/jsx-runtime');
+			return true;
+		} catch (e) {
+			return false;
+		}
+	})();
+
 	// Check if TypeScript is setup
 	const useTypeScript = fs.existsSync('tsconfig.json');
 
@@ -463,7 +477,13 @@ module.exports = function (env) {
 				resolvePluginsRelativeTo: __dirname,
 				// @remove-on-eject-begin
 				baseConfig: {
-					extends: [require.resolve('eslint-config-enact')]
+					extends: [require.resolve('eslint-config-enact')],
+					rules: {
+						...(!hasJsxRuntime && {
+							'react/jsx-uses-react': 'warn',
+							'react/react-in-jsx-scope': 'warn'
+						})
+					}
 				},
 				useEslintrc: false,
 				// @remove-on-eject-end
