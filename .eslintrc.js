c683cbb65ab7789d3df52b344174ac53e37fcacc
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-05-24 18:18:07 +0300

    added jsx-a11y module

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
 	env: {
 		node: true
 	},
-	extends: ['enact', 'plugin:prettier/recommended', 'prettier'],
-	plugins: ['import'],
+	extends: ['enact', 'plugin:prettier/recommended', 'prettier', 'plugin:jsx-a11y/strict'],
+	plugins: ['import', 'jsx-a11y'],
 	rules: {
 		'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
 		'import/named': 'error',
@@ -16,6 +16,16 @@ module.exports = {
 			{
 				'newlines-between': 'never',
 				groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+			}
+		],
+		'jsx-a11y/alt-text': 'warn',
+		'jsx-a11y/aria-role': 'warn',
+		'jsx-a11y/aria-props': 'warn',
+		'jsx-a11y/img-redundant-alt': [
+			'warn',
+			{
+				components: ['Image'],
+				words: ['picture', 'image']
 			}
 		]
 	}
