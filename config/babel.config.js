66dc2ebf55755afd1ada8823ed762db6b1e96226
Seungcheon Baek <sc.baek@lge.com>
2022-06-21 19:02:33 +0900

    Fix unit test failure
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- config/babel.config.js
+++ config/babel.config.js
@@ -89,6 +89,7 @@ module.exports = function (api) {
 
 			// Stage 3
 			//require('@babel/plugin-syntax-dynamic-import').default,
+			require('@babel/plugin-proposal-class-properties').default,
 			//[require('@babel/plugin-proposal-class-properties').default, {loose: true}],
 			//[require('@babel/plugin-proposal-private-methods').default, {loose: true}],
 			//[require('@babel/plugin-proposal-private-property-in-object').default, {loose: true}],
