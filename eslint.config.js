728d987f2dd074bedf4401bf760d287d274d589e
Stanca Pop <stanca.pop@lgepartner.com>
2023-02-08 14:42:33 +0200

    added new eslint.config file

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const enactPlugin = require('eslint-plugin-enact');
+const importPlugin = require('eslint-plugin-import');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierRecommendedPlugin = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
+
+module.exports = [
+	{
+		files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+		languageOptions: {
+		  ecmaVersion: 'latest',
+		  sourceType: 'module',
+		  globals: {
+			  ...globals.browser,
+		  },
+		  parserOptions: {
+			  ecmaFeatures: {
+				  jsx: true,
+			  },
+		  }
+		},
+		plugins: {
+			enactPlugin,
+			importPlugin,
+			prettierPlugin,
+			prettierRecommendedPlugin
+		},
+		rules: {
+		  'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
+		  'import/named': 'error',
+		  'import/first': 'warn',
+		  'import/no-duplicates': 'error',
+		  'import/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
+		  'import/newline-after-import': 'warn',
+		  'import/order': [
+			  'warn',
+			  {
+				  'newlines-between': 'never',
+				  groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+			  }
+		  ]
+		}
+	}
+];
