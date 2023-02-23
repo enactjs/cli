e4c19a30ded526af7ff6d455ba36fc484451b84b
Stanca Pop <stanca.pop@lgepartner.com>
2023-02-23 16:31:13 +0200

    upgraded eslint-plugin-react dependency version and replaced .eslintrc.js with eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+const enactConfig = require('eslint-config-enact');
+const importPlugin = require('eslint-plugin-import');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierRecommended = require('eslint-config-prettier');
+
+module.exports = [
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
+		},
+		plugins: {
+			enactConfig,
+			import: importPlugin,
+			prettierPlugin,
+			prettierRecommended
+		},
+		rules: {
+			'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
+			'import/named': 'error',
+			'import/first': 'warn',
+			'import/no-duplicates': 'error',
+			'import/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
+			'import/newline-after-import': 'warn',
+			'import/order': [
+				'warn',
+				{
+					'newlines-between': 'never',
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+				}
+			],
+			'prettierPlugin/prettier': 'error',
+			'prettierRecommended/arrow-body-style': 'off',
+			'prettierRecommended/prefer-arrow-callback': 'off'
+		}
+	}
+];
