5d4b347de4dca018ebc6922a5ae68c9cf56b9e5b
taeyoung.hong <taeyoung.hong@lge.com>
2024-06-24 19:58:18 +0900

    test: move options into module.css test directly 2

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -356,7 +356,11 @@ module.exports = function (
 							use: getLessStyleLoaders({
 								importLoaders: 2,
 								modules: {
-									getLocalIdent
+									getLocalIdent,
+									// Options to restore 6.x behavior:
+									// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
+									namedExport: false,
+									exportLocalsConvention: 'as-is'
 								}
 							})
 						},
@@ -365,7 +369,11 @@ module.exports = function (
 							use: getLessStyleLoaders({
 								importLoaders: 2,
 								modules: {
-									...(app.forceCSSModules ? {getLocalIdent} : {mode: 'icss'})
+									...(app.forceCSSModules ? {getLocalIdent} : {mode: 'icss'}),
+									// Options to restore 6.x behavior:
+									// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
+									namedExport: false,
+									exportLocalsConvention: 'as-is'
 								}
 							}),
 							sideEffects: true
@@ -377,7 +385,11 @@ module.exports = function (
 							use: getScssStyleLoaders({
 								importLoaders: 3,
 								modules: {
-									getLocalIdent
+									getLocalIdent,
+									// Options to restore 6.x behavior:
+									// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
+									namedExport: false,
+									exportLocalsConvention: 'as-is'
 								}
 							})
 						},
@@ -387,7 +399,11 @@ module.exports = function (
 							use: getScssStyleLoaders({
 								importLoaders: 3,
 								modules: {
-									...(app.forceCSSModules ? {getLocalIdent} : {mode: 'icss'})
+									...(app.forceCSSModules ? {getLocalIdent} : {mode: 'icss'}),
+									// Options to restore 6.x behavior:
+									// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
+									namedExport: false,
+									exportLocalsConvention: 'as-is'
 								}
 							})
 						},
