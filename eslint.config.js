b82b2d62944c969ab50d9acf3ade9db08d99229b
Stanca <stanca.pop@lgepartner.com>
2024-10-22 15:16:37 +0300

    refactored eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,59 +1,46 @@
-import _import from "eslint-plugin-import";
-import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+const enactConfig = require('eslint-config-enact');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
-
-export default [...compat.extends("enact", "plugin:prettier/recommended", "prettier"), {
-	plugins: {
-		import: fixupPluginRules(_import),
-	},
-
-	languageOptions: {
-		globals: {
-			...globals.node,
+module.exports = [
+	enactConfig,
+	eslintPluginPrettierRecommended,
+	eslintConfigPrettier,
+	{
+		plugins: {
+			import: eslintPluginImport,
 		},
-
-		ecmaVersion: "latest",
-		sourceType: "module",
-
-		parserOptions: {
-			ecmaFeatures: {
-				jsx: true,
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+			ecmaVersion: "latest",
+			sourceType: "module",
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
 			},
 		},
-	},
-
-	rules: {
-		"import/no-unresolved": ["error", {
-			commonjs: true,
-			caseSensitive: true,
-		}],
-
-		"import/named": "error",
-		"import/first": "warn",
-		"import/no-duplicates": "error",
-
-		"import/extensions": ["warn", "always", {
-			js: "never",
-			json: "always",
-		}],
-
-		"import/newline-after-import": "warn",
-
-		"import/order": ["warn", {
-			"newlines-between": "never",
-			groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-		}],
-	},
-}];
+		rules: {
+			"import/no-unresolved": ["error", {
+				commonjs: true,
+				caseSensitive: true,
+			}],
+			"import/named": "error",
+			"import/first": "warn",
+			"import/no-duplicates": "error",
+			"import/extensions": ["warn", "always", {
+				js: "never",
+				json: "always",
+			}],
+			"import/newline-after-import": "warn",
+			"import/order": ["warn", {
+				"newlines-between": "never",
+				groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+			}],
+		},
+	}
+];
