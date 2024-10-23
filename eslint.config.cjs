d832bc0fd2d3be0bc466ff9f056360b0bafe726f
Stanca <stanca.pop@lgepartner.com>
2024-10-23 14:29:20 +0300

    test

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,15 +1,8 @@
-const _import = require("eslint-plugin-import");
-
-const {
-    fixupPluginRules,
-} = require("@eslint/compat");
-
-const globals = require("globals");
-const js = require("@eslint/js");
-
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const _import = require('eslint-plugin-import');
+const {fixupPluginRules} = require('@eslint/compat');
+const globals = require('globals');
+const js = require('@eslint/js');
+const {FlatCompat} = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
     baseDirectory: __dirname,
@@ -17,46 +10,51 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-module.exports = [...compat.extends("enact", "plugin:prettier/recommended", "prettier"), {
-    plugins: {
-        import: fixupPluginRules(_import),
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
+module.exports = [
+    ...compat.extends('enact', 'plugin:prettier/recommended', 'prettier'),
+    {
+        plugins: {
+            import: fixupPluginRules(_import)
         },
-
-        ecmaVersion: "latest",
-        sourceType: "module",
-
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
+        languageOptions: {
+            globals: {
+                ...globals.node
             },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
         },
-    },
-
-    rules: {
-        "import/no-unresolved": ["error", {
-            commonjs: true,
-            caseSensitive: true,
-        }],
-
-        "import/named": "error",
-        "import/first": "warn",
-        "import/no-duplicates": "error",
-
-        "import/extensions": ["warn", "always", {
-            js: "never",
-            json: "always",
-        }],
-
-        "import/newline-after-import": "warn",
-
-        "import/order": ["warn", {
-            "newlines-between": "never",
-            groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-        }],
-    },
-}];
+        rules: {
+            'import/no-unresolved': [
+                'error',
+                {
+                    commonjs: true,
+                    caseSensitive: true
+                }
+            ],
+            'import/named': 'error',
+            'import/first': 'warn',
+            'import/no-duplicates': 'error',
+            'import/extensions': [
+                'warn',
+                'always',
+                {
+                    js: 'never',
+                    json: 'always'
+                }
+            ],
+            'import/newline-after-import': 'warn',
+            'import/order': [
+                'warn',
+                {
+                    'newlines-between': 'never',
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+                }
+            ]
+        }
+    }
+];
