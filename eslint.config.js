fa18feb87d5a8052011b7324a4f616f835db9238
Stanca <stanca.pop@lgepartner.com>
2024-11-19 18:26:27 +0200

    updated eslint to version 9 and added new config file

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+// const enactConfig = require('eslint-config-enact');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
+
+module.exports = [
+	// enactConfig,
+	eslintPluginPrettierRecommended,
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.node
+			},
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
+		},
+		plugins: {
+			import: eslintPluginImport
+		},
+		rules: {
+			'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
+			'import/named': 'error',
+			'import/first': 'warn',
+			'import/no-duplicates': 'error',
+			'import/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
+			'import/newline-after-import': 'warn',
+			'import/order': [
+				'warn',
+				{
+					'newlines-between': 'never',
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+				}
+			]
+		}
+	}
+];
