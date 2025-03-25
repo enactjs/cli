428113424e16e6f2b591c5f71ac0a3f580cb9a13
Mikyung Kim <mikyung27.kim@lge.com>
2025-03-25 16:12:03 +0900

    WRR-21058: Added support importing JSON files in CSS
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -148,7 +148,17 @@ module.exports = function (
 							// the browserslist targets.
 							!useTailwind && require('postcss-normalize'),
 							// Resolution indepedence support
-							app.ri !== false && require('postcss-resolution-independence')(app.ri)
+							app.ri !== false && require('postcss-resolution-independence')(app.ri),
+							// Support importing JSON files in CSS
+							[
+								'@daltontan/postcss-import-json',
+								{
+									map: (selector, value) => {
+										// Convert JSON values to CSS variables
+										return String(value).startsWith('ref') ? `var(--${value});` : value;
+									}
+								}
+							]
 						].filter(Boolean)
 					},
 					sourceMap: shouldUseSourceMap
